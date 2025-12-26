1285f16c8a161c0c018f7d5dae0ddcc07c07274a
Astakh1 <ivan2008.astahov@gmail.com>
2025-12-26 15:17:33 +0300

    broke compilation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use temperature_converter::convert_temperature;
 
 fn main() {
     println!("30°C = {} K", convert_temperature(30.0, "C", "K").unwrap());
+    Uncompilable
     println!("100°F = {} K", convert_temperature(100.0, "F", "K").unwrap());
     println!("-10°C = {} °F", convert_temperature(-10.0, "C", "F").unwrap());
 }
